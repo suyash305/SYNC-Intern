6bd1e01baa9472ccc32ac51e4ef75b562bd1c88f
suyash305 <119748244+suyash305@users.noreply.github.com>
2023-08-02 21:29:07 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,80 @@
+const Courses = [
+    {
+      name: "HTML",
+      img: "./images/html.png",
+      video: "./videos/HTML.mp4",
+      discription:
+        "This course has everything you need to learn how write HTML code from scratch. Fast paced get to the point training, to learn about creating websites and using HTML code. By the end of the course you will know how to apply HTML to make a real website.",
+      assignment:
+        "create Sign up form which takes full needed information about the customer.",
+      teacher: "Bro Code",
+      teacherImg: "./images/teacher1.png",
+    },
+    {
+      name: "CSS",
+      img: "images/css.png",
+      video: "videos/CSS.mp4",
+      discription:
+        "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.",
+      assignment:
+        "create a page that has categories use Flexbox when designing it.",
+      teacher: "Slaying The Dragon",
+      teacherImg: "images/teacher4.png",
+    },
+    {
+      name: "JavaScript",
+      img: "images/js.png",
+      video: "videos/JavaScript.mp4",
+      discription:
+        "Students learn the basics of JavaScript including variables, user input, mathematics, and basic graphics. Students learn how to pair program while tackling more challenging tasks with JavaScript graphics.",
+      assignment: "make a Quiz app using Js.",
+      teacher: "Fireship",
+      teacherImg: "images/teacher3.png",
+    },
+    {
+      name: "Bootstrap5",
+      img: "images/bootstrap.png",
+      video: "videos/Bootstrap5.mp4",
+      discription:
+        "Learning Bootstrap CSS allows you to create ultra responsive, mobile first projects. It provides style definitions through HTML for consistency, allowing developers to create seamless builds across multiple platforms.",
+      assignment:
+        "create a Product Landing page using HTML, Bootstrap, and js only.",
+      teacher: "Elzero",
+      teacherImg: "images/teacher2.png",
+    },
+    {
+      name: "Node-js",
+      img: "images/nodeJS.png",
+      video: "videos/Node-js.mp4",
+      discription:
+        "In this course, you'll learn how to develop MongoDB backed applications, implement testing, build microservices applications, and build a real-time chat app using Socket IO.",
+      assignment:
+        "create a Travilling website that has a database of Customer info.",
+      teacher: "Codevolution",
+      teacherImg: "images/teacher5.png",
+    },
+    {
+      name: "JQuery",
+      img: "images/jquery.png",
+      video: "videos/jQuery.mp4",
+      discription:
+        "jQuery is a fast, lightweight multi-browser JavaScript library. The Best part of jQuery is that Interacts with the DOM, makes Ajax requests and also creates effects even after javascript is been disabled in the browser",
+      assignment: "create a Snake Game with jQuery.",
+      teacher: "Yahoo Baba",
+      teacherImg: "images/teacher6.png",
+    },
+  ];
+  let Users = [
+    {
+      firstname: "xyz",
+      lastname: "abc",
+      email: "xyz@gmail.com",
+      pass: "12345",
+    }
+  ];
+  const Admin = [
+    {
+      email: "admin@gmail.com",
+      password: "123",
+    },
+  ];
--- script.js
+++ script.js
@@ -1,111 +1,249 @@
-console.log("Welcome to Spotify");
+let courseNameEl = document.querySelectorAll(".course");
+let courseImg = document.querySelectorAll(".courseImg");
+let courseTeacher = document.getElementById("teacher");
+let TeacherImage = document.getElementById("imgTeacher");
+let description = document.getElementById("discription");
+let ass = document.getElementById("ass");
+let Video = document.getElementById("video");
+let part = document.getElementById("part");
+let CheckEmail = document.getElementById("CheckEmail");
+let CheckPass = document.getElementById("CheckPass");
+let firstName = document.getElementById("firstName");
+let LastName = document.getElementById("LastName");
+let AddEmail = document.getElementById("AddEmail");
+let AddPass = document.getElementById("AddPass");
+let courseInd = 0;
 
-// Initialize the Variables
-let songIndex = 0;
-let audioElement = new Audio('songs/1.mp3');
-let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar');
-let gif = document.getElementById('gif');
-let masterSongName = document.getElementById('masterSongName');
-let songItems = Array.from(document.getElementsByClassName('songItem'));
-
-let songs = [
-    {songName: " On my way - Alan Walker ", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: " Faded -Alan Walker ", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: " On and On - Cartoon ", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Warriyo - Mortals ", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Gangasters Paradise - Coolio ", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Munda Sona hoon main ", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Tere pyaar mein ", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Maan Meri Jaan - King ", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Jhoome Jo Pathaan", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Pyaar hota kayi baar hai", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
-]
-
-songItems.forEach((element, i)=>{ 
-    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
-})
- 
-
-// Handle play/pause click
-masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-        gif.style.opacity = 1;
+function addUser() {
+  if (
+    firstName.value !== "" &&
+    LastName.value !== "" &&
+    AddEmail.value !== "" &&
+    AddPass.value !== ""
+  ) {
+    Users.push({
+      firstname: firstName.value,
+      lastname: LastName.value,
+      email: AddEmail.value,
+      pass: AddPass.value,
+    });
+    sessionStorage.setItem("users", JSON.stringify(Users));
+    sessionStorage.setItem("user", JSON.stringify(AddEmail.value));
+    firstName.value = "";
+    LastName.value = "";
+    AddEmail.value = "";
+    AddPass.value = "";
+    console.log(Users);
+  } else {
+    alert("All Data are Required!");
+  }
+}
+function checkInfo() {
+  if (CheckEmail.value !== "" && CheckPass.value !== "") {
+    console.log(CheckEmail.value, CheckPass.value);
+    const isAdmin = Admin.some(
+      (user) =>
+        user.email === CheckEmail.value && user.password === CheckPass.value
+    );
+    console.log(isAdmin);
+    if (isAdmin) {
+      window.location.href = "../admin.html";
+    } else {
+      let sessionStorageUsers = sessionStorage.getItem("users");
+      let users = sessionStorageUsers ? JSON.parse(sessionStorageUsers) : Users;
+      console.log(users[0]);
+      const isUser = users.some(
+        (user) =>
+          user.email === CheckEmail.value && user.pass === CheckPass.value
+      );
+      console.log(isUser);
+      if (isUser) {
+        sessionStorage.setItem("Showing", JSON.stringify(true));
+        window.location.href = "../Courses.html";
+      } else {
+        alert("Email or Password is incorrect");
+      }
     }
-    else{
-        audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
-        gif.style.opacity = 0;
-    }
-})
-// Listen to Events
-audioElement.addEventListener('timeupdate', ()=>{ 
-    // Update Seekbar
-    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
-    myProgressBar.value = progress;
-})
+  } else alert("Email and Password are both required!");
+}
+function gotoCourse(num) {
+  courseInd = num;
+  document.querySelector(".coursesBody").style.display = "none";
+  document.querySelector(".playlistBody").style.display = "block";
+  ass.innerHTML = Courses[courseInd].assignment;
+  TeacherImage.src = Courses[courseInd].teacherImg;
+  courseTeacher.innerHTML = Courses[courseInd].teacher;
+  description.innerHTML = Courses[courseInd].discription;
+  courseImg.forEach((img) => (img.src = Courses[courseInd].img));
+  courseNameEl.forEach((el) => (el.innerHTML = Courses[courseInd].name));
 
-myProgressBar.addEventListener('change', ()=>{
-    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
-})
-
-const makeAllPlays = ()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-        element.classList.remove('fa-pause-circle');
-        element.classList.add('fa-play-circle');
-    })
+  let IsLogin = JSON.parse(sessionStorage.getItem("Showing"));
+  console.log(IsLogin);
+  if (IsLogin === true) {
+    document.querySelector(".ass").style.display = "block";
+  } else {
+    document.querySelector(".ass").style.display = "none";
+  }
+}
+function gotoVideo(num) {
+  event.preventDefault();
+  document.querySelector(".playlistBody").style.display = "none";
+  document.querySelector(".videoBody").style.display = "block";
+  Video.src = Courses[courseInd].video;
+  part.innerHTML = num;
 }
 
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click', (e)=>{ 
-        makeAllPlays();
-        songIndex = parseInt(e.target.id);
-        e.target.classList.remove('fa-play-circle');
-        e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
-    })
-})
+let Assignment = [];
+function addAss() {
+  if (
+    document.getElementById("stuName").value !== "" &&
+    document.getElementById("file").value !== "" &&
+    document.getElementById("courseName").value !== ""
+  ) {
+    const newElement = {
+      StudentName: document.getElementById("stuName").value,
+      AssignmentFile: document.getElementById("file").value,
+      CourseName: document.getElementById("courseName").value,
+    };
+    console.log(newElement.StudentName);
+    Assignment.push(newElement);
+    document.getElementById("stuName").value = "";
+    document.getElementById("file").value = "";
+    document.getElementById("courseName").value = "";
+    document.querySelector(".sucess").style.display = "block";
+    setTimeout(() => {
+      document.querySelector(".sucess").style.display = "none";
+      const index = localStorage.length;
+      localStorage.setItem(`Assignment[${index}]`, JSON.stringify(newElement));
+      console.log(JSON.parse(localStorage.getItem(`Assignment[${index}]`)));
+    }, 2000);
+  } else {
+    alert("Student Name, File, and Course Name are all required!");
+  }
+  console.log(Assignment);
+}
+function showTable() {
+  const table = document.getElementById("assTable");
+  let data = [];
+  while (table.rows.length > 1) {
+    table.deleteRow(-1);
+  }
+  for (let j = 0; j < localStorage.length; j++) {
+    const key = localStorage.key(j);
 
-document.getElementById('next').addEventListener('click', ()=>{
-    if(songIndex>=9){
-        songIndex = 0
+    if (key.includes("Assignment[")) {
+      try {
+        const element = JSON.parse(localStorage.getItem(key));
+        data.push(element);
+
+        let trEl = `
+          <tr>
+            <td>${element.StudentName}</td>
+            <td>${element.CourseName}</td>
+            <td><a>${element.AssignmentFile}</a></td>
+          </tr>
+        `;
+        table.insertAdjacentHTML("beforeend", trEl);
+      } catch (error) {
+        console.error(`Error parsing JSON for ${key}: ${error}`);
+      }
     }
-    else{
-        songIndex += 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.add('fa-pause-circle');
+  }
+  table.style.display = "table";
+}
+var barChartOptions = {
+  series: [
+    {
+      data: [12, 10, 8, 6, 4, 2],
+    },
+  ],
+  chart: {
+    type: "bar",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#cc3c43", "#246dec", "#f5b74f", "#4f35a1", "#367952", "#246dec"],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  xaxis: {
+    categories: ["HTML", "CSS", "JS", "Bootstrap", "Node js", "JQuery"],
+  },
+  yaxis: {
+    title: {
+      text: "Watches",
+    },
+  },
+};
 
-})
+var barChart = new ApexCharts(
+  document.querySelector("#barChart"),
+  barChartOptions
+);
+barChart.render();
 
-document.getElementById('previous').addEventListener('click', ()=>{
-    if(songIndex<=0){
-        songIndex = 0
-    }
-    else{
-        songIndex -= 1;
-    }
-    audioElement.src = `songs/${songIndex+1}.mp3`;
-    masterSongName.innerText = songs[songIndex].songName;
-    audioElement.currentTime = 0;
-    audioElement.play();
-    masterPlay.classList.remove('fa-play-circle');
-    masterPlay.classList.add('fa-pause-circle');
-})
-
-
+var areaChartOptions = {
+  series: [
+    {
+      name: "This Year",
+      data: [31, 40, 28, 51, 42, 109, 100],
+    },
+    {
+      name: "Last Year",
+      data: [11, 32, 45, 32, 34, 52, 41],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "area",
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#367952", "#f5b74f"],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  markers: {
+    size: 0,
+  },
+  yaxis: [
+    {
+      title: {
+        text: "Last Year",
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: "This Year",
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+  },
+};
+var areaChart = new ApexCharts(
+  document.querySelector("#areaChart"),
+  areaChartOptions
+);
+areaChart.render();
